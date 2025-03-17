95a5f3de50d9f645b46158d358920ff85936eb1a
akshoaib <akhtarshoaib317@gmail.com>
2025-03-17 14:28:08 +0500

    vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
