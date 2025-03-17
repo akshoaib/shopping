5b10e5fd453afaac25168463148464749fecd747
akshoaib <akhtarshoaib317@gmail.com>
2025-03-17 15:07:12 +0500

    removed tailwind

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,14 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./",
-  // build: {
-  //   outDir: "dist",
-  //   assetsDir: "assets",
-  // },
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 });
-// import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite";
-// export default defineConfig({
-//   plugins: [tailwindcss()],
-// });
