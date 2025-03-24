d0c0dd83a9f7685f6d0d521e269b344470bf72e4
akshoaib <88835537+akshoaib@users.noreply.github.com>
2025-03-24 21:33:59 +0500

    login icon added

--- src/components/shared-components/guest-layout/header/index.jsx
+++ src/components/shared-components/guest-layout/header/index.jsx
@@ -2,6 +2,7 @@ import { IoCartOutline } from "react-icons/io5";
 import styles from "./header.module.css";
 import { useNavigate } from "react-router-dom";
 import { APP_ROUTES } from "@/config/app-routes";
+import { LuLogIn } from "react-icons/lu";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -9,9 +10,19 @@ const Header = () => {
   return (
     <div className={styles.productsHeader}>
       <p>WardrobeWave</p>
-      <p onClick={() => navigate(APP_ROUTES.public.CART)}>
-        <IoCartOutline role="button" size={30} color="#ffffff" />
-      </p>
+      <div className="d-flex gap-2">
+        <p>
+          <LuLogIn
+            role="button"
+            size={25}
+            color="#ffffff"
+            onClick={() => navigate(APP_ROUTES.public.LOGIN)}
+          />
+        </p>
+        <p onClick={() => navigate(APP_ROUTES.public.CART)}>
+          <IoCartOutline role="button" size={30} color="#ffffff" />
+        </p>
+      </div>
     </div>
   );
 };
