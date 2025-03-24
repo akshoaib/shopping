65179466a5781dce80396fdc5b0e38d93a6fddad
akshoaib <88835537+akshoaib@users.noreply.github.com>
2025-03-24 21:56:57 +0500

    categories fix

--- src/components/category/CategoryBox/index.jsx
+++ src/components/category/CategoryBox/index.jsx
@@ -9,7 +9,12 @@ const CategoryBox = ({ category, handleCategoryClick }) => {
         className={`${styles.categoryBox}`}
         onClick={() => handleCategoryClick(category._id)}
       >
-        <img width={"100%"} src={category?.image} alt={category?.name} />
+        <img
+          width={"100%"}
+          height={200}
+          src={category?.image}
+          alt={category?.name}
+        />
         <div className={styles.categoryBoxContent}>
           <p>{category?.name}</p>
         </div>
--- src/components/homepage/product-categories/index.jsx
+++ src/components/homepage/product-categories/index.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import ProductCategoriesSkeleton from "./product-categories-skeleton";
 import { APP_ROUTES } from "@/config/app-routes";
+import { Col, Row } from "antd";
 
 const ProductCategories = ({ loading, categories }) => {
   const navigate = useNavigate();
@@ -9,26 +10,30 @@ const ProductCategories = ({ loading, categories }) => {
       {loading ? (
         <ProductCategoriesSkeleton />
       ) : (
-        categories?.map((category) => (
-          <div key={category.id} className="">
-            {console.log("category", category)}
-            <div
-              onClick={() =>
-                navigate(APP_ROUTES.public.PRODUCTS_LISTING(category._id))
-              }
-              className="p-2 "
-            >
-              <img
-                src={category.image}
-                alt={category.name}
-                width={100}
-                height={100}
-                className="rounded-circle"
-              />
-              <p className="text-center">{category.name}</p>
-            </div>
-          </div>
-        ))
+        <Row className="w-100 justify-content-around justify-content-md-center">
+          {categories?.map((category) => (
+            <Col xs={9} md={4}>
+              <div key={category.id} className="">
+                {console.log("category", category)}
+                <div
+                  onClick={() =>
+                    navigate(APP_ROUTES.public.PRODUCTS_LISTING(category._id))
+                  }
+                  className="p-2 d-flex flex-column justify-content-center"
+                >
+                  <img
+                    src={category.image}
+                    alt={category.name}
+                    width={100}
+                    height={100}
+                    className="rounded-circle mx-auto"
+                  />
+                  <p className="text-center">{category.name}</p>
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
       )}
     </>
   );
