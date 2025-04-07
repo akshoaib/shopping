8020026989c8c6415263eb5790f07d82fa1543b0
akshoaib <akhtarshoaib317@gmail.com>
2025-04-07 11:35:40 +0500

    sign in design change

--- src/components/signin/index.jsx
+++ src/components/signin/index.jsx
@@ -9,6 +9,8 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { Col, Row } from "antd";
 import PageLoader from "../shared-components/page-loader";
+import LogoImage from "../../assets/logo.png";
+import { APP_ROUTES } from "@/config/app-routes";
 
 const Signin = () => {
   const { loading, signin } = useAuth();
@@ -52,7 +54,14 @@ const Signin = () => {
     <>
       <PageLoader loading={loading} />
       <Row className={styles.signinContainer}>
-        <Col span={24} className="h-50 bg-dark"></Col>
+        <Col
+          span={24}
+          className="h-50 bg-dark d-flex justify-content-center align-items-center"
+        >
+          <div onClick={() => navigate(APP_ROUTES.public.HOME)} role="button">
+            <img src={LogoImage} width={200} />
+          </div>
+        </Col>
         <Col span={24} className="px-4 mb-5">
           <Row className="mb-5">
             <Col xs={24} md={12} className="mx-auto mb-5">
