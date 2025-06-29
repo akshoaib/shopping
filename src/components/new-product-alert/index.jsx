47c818e83a22ff2cbcfac1dcc0df912b18937633
akshoaib <88835537+akshoaib@users.noreply.github.com>
2025-06-29 21:07:48 +0500

    sockets fixed

--- src/components/new-product-alert/index.jsx
+++ src/components/new-product-alert/index.jsx
@@ -6,6 +6,7 @@ const NewProductAlert = ({
   image,
   price,
   handleCloseAlert,
+  productId,
 }) => {
   return (
     <Row
@@ -15,32 +16,44 @@ const NewProductAlert = ({
         left: "50%",
         transform: "translateX(-50%)",
         width: "40%",
-        margin: "10px auto !important",
+        margin: "15px auto !important",
         zIndex: 1000,
         backgroundColor: "white",
-        padding: "10px",
         boxShadow: "0 2px 8px rgba(0, 0, 0, 0.15)",
         borderRadius: "8px",
+        cursor: "pointer",
       }}
       className="new-product-alert-container"
+      onClick={(e) => {
+        e.stopPropagation();
+        window.location.href = `/product/${productId}`;
+      }}
     >
-      <Col span={6} className="new-product-alert">
+      <Col span={4} className="new-product-alert">
         <img
-          width={50}
-          height={50}
+          width={"100%"}
+          height={"100%"}
           src={image}
           alt={title}
           className="rounded"
         />
       </Col>
 
-      <Col span={16} className="new-product-alert">
+      <Col span={19} className="new-product-alert pt-2 ps-2">
         <h6>{title}</h6>
         <p>{description}</p>
         <p>Price: {price} Rs.</p>
       </Col>
-      <Col span={2}>
-        <button onClick={() => handleCloseAlert()}>X</button>
+      <Col span={1} className="d-flex justify-content-end align-items-start">
+        <button
+          className="rounded p-1 bg-transparent border-0"
+          onClick={(e) => {
+            e.stopPropagation();
+            handleCloseAlert();
+          }}
+        >
+          X
+        </button>
       </Col>
     </Row>
   );
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,14 +6,41 @@ import { useSelector } from "react-redux";
 import ProtectedAdminRoutes from "./protected-admin-routes";
 import { roles } from "@/constants";
 import ProtectedUserRoutes from "./protected-user-routes";
+import { useEffect, useState } from "react";
+import NewProductAlert from "@/components/new-product-alert";
+import { socket } from "@/services/socket";
 
 const AllRoutes = () => {
   const token = useSelector((state) => state.auth.token);
   const user = useSelector((state) => state.auth.loggedInUser);
+  const [newProduct, setNewProduct] = useState(null);
+
+  console.log({ user });
+
+  useEffect(() => {
+    socket.on("new-product", (product) => {
+      setNewProduct(product);
+    });
+
+    return () => {
+      socket.off("new-product");
+    };
+  }, []);
+  console.log({ newProduct });
 
   return (
     <Router>
       <ToastContainer />
+      {newProduct && user?.role !== "admin" && (
+        <NewProductAlert
+          title={newProduct.name}
+          description={newProduct.description}
+          image={newProduct.images[0]}
+          price={newProduct.price}
+          productId={newProduct._id}
+          handleCloseAlert={() => setNewProduct(null)}
+        />
+      )}
       {token ? (
         user.role === roles.ADMIN ? (
           <ProtectedAdminRoutes />
