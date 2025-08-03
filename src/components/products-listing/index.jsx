e3e5095053d6a495530f9ad5e39c8767536f6305
akshoaib <88835537+akshoaib@users.noreply.github.com>
2025-08-03 22:45:57 +0500

    clickable product card

--- src/components/products-listing/index.jsx
+++ src/components/products-listing/index.jsx
@@ -1,12 +1,14 @@
 import { Col, Row } from "antd";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import ProductCard from "../shared-components/product-card";
 import useProduct from "@/hooks/useProduct";
 import PageLoader from "../shared-components/page-loader";
+import { APP_ROUTES } from "@/config/app-routes";
 
 const ProductsListing = () => {
   const { categoryId } = useParams();
+  const navigate = useNavigate();
   const { loading, getProductsByCategory } = useProduct();
   const [products, setProducts] = useState([]);
 
@@ -37,7 +39,17 @@ const ProductsListing = () => {
           <Row gutter={[16, 16]} className="mb-4">
             {products.map((product) => {
               return (
-                <Col xs={12} md={8} lg={6} xl={4}>
+                <Col
+                  xs={12}
+                  md={8}
+                  lg={6}
+                  xl={4}
+                  role="button"
+                  key={`${product._id}`}
+                  onClick={() =>
+                    navigate(APP_ROUTES.public.VIEW_PRODUCT(product._id))
+                  }
+                >
                   <ProductCard product={product} />
                 </Col>
               );
